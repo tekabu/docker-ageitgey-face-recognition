b800f2b5bbec0165f2f7ca99a2891f8129b19ab9
Nino <tekabu0622@gmail.com>
2025-05-18 00:53:35 +0800

    Add Dockerfile and build scripts for face recognition project

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t ageitgey_face_recognition .
---
+++ docker_hub.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker tag ageitgey_face_recognition:latest ninoescalera04/ageitgey_face_recognition:latest
+docker push ninoescalera04/ageitgey_face_recognition:latest
